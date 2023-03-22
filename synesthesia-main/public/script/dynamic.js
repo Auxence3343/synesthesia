62b281a9ea5c95d028b22fb49c10305e31938d4d
celine <celin@DESKTOP-RTFAAU6>
2023-03-22 16:17:50 +0100

    Modif

--- synesthesia-main/public/script/dynamic.js
+++ synesthesia-main/public/script/dynamic.js
@@ -1,11 +1,12 @@
 var sliderQ = document.getElementById("Quality");
 var outputQ = document.getElementById("QualityD");
 outputQ.innerHTML = sliderQ.value * 2;
-
+    
+console.log("test");
 var sliderW = document.getElementById("Accuracy");
 var outputW = document.getElementById("AccuracyD");
 outputW.innerHTML = sliderW.value * 2;
-            
+    
 var sliderC = document.getElementById("Quickness");
 var outputC = document.getElementById("QuicknessD");
 outputC.innerHTML = sliderC.value * 2;
